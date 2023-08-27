5dfcd74713db59cd1e2d67ad5b6987c09ef29863
galemwendar <galemwendar@gmail.com>
2023-08-27 11:23:14 +0300

    Initial settings

--- yadownloader.py
+++ yadownloader.py
@@ -2,6 +2,9 @@ import os
 import requests
 import sys
 import urllib.parse
+import humanfriendly
+import re
+from tqdm import tqdm
 
 if len(sys.argv) < 2:
   print('Usage: yadowmloader  <public_url_from_yandex_disk>')
@@ -18,29 +21,25 @@ def download_file_from_disk(public_url):
   #получаем настоящую ссылку
   head = requests.head(api_url)
   download_url = head.headers['Location']
-  
-
-  print('Заголовки ответа:')
-  
   head = requests.head(download_url)
-  
-  size = int(head.headers['Content-Length'])
-  print(size)
-      
+  file_size = int(head.headers['Content-Length'])
+  size = humanfriendly.format_size(file_size)   
   name = head.headers['Content-Disposition'].split(';')[1].split('=')[1]
   name = urllib.parse.unquote(name)
-  print(name)
+  name = re.sub('^UTF-8\'\'', '', name)
+  
+  print(name,size)
   
   choice = input('Хотите скачать файл? (y/n): ')
   if choice.lower() != 'y':
     sys.exit()
   
-  response = requests.get(download_url)
-  
-  with open(file_name, 'wb') as f:
-    f.write(response.content)
+  response = requests.get(download_url,stream=True)
+  with open(name, 'wb') as f:
+    for data in tqdm(response.iter_content(), total=file_size, unit='B', unit_scale=True):
+       f.write(data)
     
-  print(f'Файл {file_name} скачан')
+  print(f'Файл {name} скачан')
 
 if __name__ == '__main__':
   public_url = sys.argv[1]
